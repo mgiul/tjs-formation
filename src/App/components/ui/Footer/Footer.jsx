30e630bb6ff4f5d58d91bfcb2804cca902757273
mgiul <fa.elkiri@gmail.com>
2023-01-23 17:34:33 +0100

    React CLI + template pour créer des components

---
+++ src/App/components/ui/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './Footer.module.css';
+
+const Footer = (props) => {
+    return <div className={style.Footer} data-testid="Footer">Footer</div>
+};
+
+export default Footer;
---
+++ src/App/components/ui/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './Header.module.css';
+
+const Header = (props) => {
+    return <div className={style.Header} data-testid="Header">Header</div>
+};
+
+export default Header;
