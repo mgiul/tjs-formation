b2a48bed19238fd19178e24233aa41970194a09b
mgiul <fa.elkiri@gmail.com>
2023-01-23 14:35:29 +0100

    creation du bouton

---
+++ src/App/components/ui/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Button = () => {
+    return <button className="Button">benjamin</button>;
+}
+
+// function Button() {
+//     return <button className="Button">benjamin</button>;
+// }
+
+export default Button;
