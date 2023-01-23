c2b7cb2a5a4d5515d48f67fbcca24a552438fb12
mgiul <fa.elkiri@gmail.com>
2023-01-23 14:39:49 +0100

    mise en oeuvre du comp button

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
+import Button from './components/ui/Button/Button';
 
 function App() {
   return (
     <div className="App">
       DEMAT BREIZH
+      <hr/>
+      <Button/>
+      <Button/>
+      <Button/>
+      
+
     </div>
   );
 }
