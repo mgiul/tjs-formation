0cf675c885ba66f302c9a266c8f9a34eec9b74f7
mgiul <fa.elkiri@gmail.com>
2023-01-24 17:11:14 +0100

    Préparation fichiers source grc

---
+++ src/App/components/layout/FlexHLayout/FlexHLayout.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import style from './FlexHLayout.module.css'
+
+export interface IFlexHLayoutProps {
+    style?: {}
+}
+
+export interface IFlexHLayoutState { }
+
+export const initialState: IFlexHLayoutState = {}
+
+const FlexHLayout: React.FC<IFlexHLayoutProps> = (props) => {
+    const [state, setstate] = useState(initialState);
+    useEffect(() => {
+        // Cmpdidmount
+    }, []);
+    return (
+        <div className={style.FlexHLayout}
+            data-testid="FlexHLayout"
+            style={props.style}>
+            templaName
+        </div>
+    );
+};
+FlexHLayout.propTypes = {
+    style: PropTypes.object,
+}
+FlexHLayout.defaultProps = {}
+
+export default FlexHLayout;
