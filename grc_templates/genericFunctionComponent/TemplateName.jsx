9ca04415f68181108e2f3b593cbbac718646d238
mgiul <fa.elkiri@gmail.com>
2023-01-24 16:56:43 +0100

    passage template ts

--- grc_templates/genericFunctionComponent/TemplateName.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './TemplateName.module.css';
-
-const TemplateName = (props) => {
-    return <div className={style.TemplateName} data-testid="TemplateName">TemplateName</div>
-};
-
-export default TemplateName;
---
+++ grc_templates/genericFunctionComponent/TemplateName.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import style from './TemplateName.module.css'
+
+export interface ITemplateNameProps {
+    style?: {}
+}
+
+export interface ITemplateNameState { }
+
+export const initialState: ITemplateNameState = {}
+
+const TemplateName: React.FC<ITemplateNameProps> = (props) => {
+    const [state, setstate] = useState(initialState);
+    useEffect(() => {
+        // Cmpdidmount
+    }, []);
+    return (
+        <div className={style.TemplateName}
+            data-testid="TemplateName"
+            style={props.style}>
+            templaName
+        </div>
+    );
+};
+TemplateName.propTypes = {
+    style: PropTypes.object,
+}
+TemplateName.defaultProps = {}
+
+export default TemplateName;
